69f3860d2884976216bb8d60e4f268a979374115
Daniel Willmann <daniel@totalueberwachung.de>
2017-05-16 14:17:08 +0200

    hlr_data.sql: Insert ki and opc instead of op to example data
    It depends on the cards whether you have op or opc, but the most cards in use
    for 3G are using the opc. Change the example to reflect that.
    
    Change-Id: I8f6051ea9b285ff6261bfe346cfc29d1167921f5

--- sql/hlr_data.sql
+++ sql/hlr_data.sql
@@ -5,9 +5,9 @@ INSERT INTO auc_2g (subscriber_id, algo_id_2g, ki) VALUES (1, 1, '00010203040506
 
 -- 3G only subscriber
 INSERT INTO subscriber (id, imsi) VALUES (2, '901990000000002');
-INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, op, sqn) VALUES (2, 5, '000102030405060708090a0b0c0d0e0f', '101112131415161718191a1b1c1d1e1f', 0);
+INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, opc, sqn) VALUES (2, 5, '000102030405060708090a0b0c0d0e0f', '101112131415161718191a1b1c1d1e1f', 0);
 
 -- 2G + 3G subscriber
 INSERT INTO subscriber (id, imsi) VALUES (3, '901990000000003');
 INSERT INTO auc_2g (subscriber_id, algo_id_2g, ki) VALUES (3, 1, '000102030405060708090a0b0c0d0e0f');
-INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, op, sqn) VALUES (3, 5, '000102030405060708090a0b0c0d0e0f', '101112131415161718191a1b1c1d1e1f', 0);
+INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, opc, sqn) VALUES (3, 5, '000102030405060708090a0b0c0d0e0f', '101112131415161718191a1b1c1d1e1f', 0);
