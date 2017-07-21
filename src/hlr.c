84201d3a4b6b745cfdb4aa3c9fb5d3e04859f89a
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2017-07-21 16:00:32 +0200

    use OSMO_GSUP_PORT == 4222 instead of hardcoded 2222
    Depends: I4222e21686c823985be8ff1f16b1182be8ad6175 (libosmocore)
    Change-Id: I9b372a4ac38677773bf813acba80cebcd88e2e20

--- src/hlr.c
+++ src/hlr.c
@@ -466,7 +466,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	g_hlr->gs = osmo_gsup_server_create(hlr_ctx, g_hlr->gsup_bind_addr, 2222,
+	g_hlr->gs = osmo_gsup_server_create(hlr_ctx, g_hlr->gsup_bind_addr, OSMO_GSUP_PORT,
 					    read_cb, &g_lu_ops);
 	if (!g_hlr->gs) {
 		LOGP(DMAIN, LOGL_FATAL, "Error starting GSUP server\n");
