3f697cdc7157883f5c9a33122a7aa1f8dc5598af
Neels Hofmeyr <neels@hofmeyr.de>
2017-10-17 22:26:00 +0200

    test_subscriber.ctrl: test against octal/hex interpretation of id
    Add a large enough subscriber id and add a test that ensures a leading zero is
    not interpreted as octal, and that a leading 0x is invalid and not interpreted
    as hexadecimal.
    
    Change-Id: Ib468b7cb595cf52331ebb41e6de0e8f57f69e173

--- tests/test_subscriber.sql
+++ tests/test_subscriber.sql
@@ -11,3 +11,7 @@ INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, opc, sqn) VALUES (2, 5, '00010
 INSERT INTO subscriber (id, imsi, msisdn) VALUES (3, '901990000000003', '103');
 INSERT INTO auc_2g (subscriber_id, algo_id_2g, ki) VALUES (3, 1, '000102030405060708090a0b0c0d0e0f');
 INSERT INTO auc_3g (subscriber_id, algo_id_3g, k, opc, sqn) VALUES (3, 5, '000102030405060708090a0b0c0d0e0f', '101112131415161718191a1b1c1d1e1f', 2342);
+
+-- A subscriber id > 7 and > 15 to check against octal and hex notations
+INSERT INTO subscriber (id, imsi, msisdn) VALUES (123, '123123', '123');
+INSERT INTO auc_2g (subscriber_id, algo_id_2g, ki) VALUES (123, 3, 'BeefedCafeFaceAcedAddedDecadeFee');
