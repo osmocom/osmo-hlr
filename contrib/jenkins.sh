4b8be4d12d2d9c6ffabc85f23e7239393c9a05fc
Oliver Smith <osmith@sysmocom.de>
2018-11-28 14:57:25 +0100

    contrib/jenkins.sh: build and publish manuals
    Add new environment variables WITH_MANUALS and PUBLISH to control if
    the manuals should be built and uploaded. Describe all environment vars
    on top of the file.
    
    When WITH_MANUALS is set, install osmo-gsm-manuals like any other
    dependency and add --enable-manuals to the configure flags (for "make"
    and "make distcheck"). Add the bin subdir of the installed files to
    PATH, so osmo-gsm-manuals-check-depends can be used by ./configure.
    
    Related: OS#3385
    Change-Id: Ia1a6ab64c7912f30a693ca4f5f474f22d72b8873

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # jenkins build helper script for osmo-hlr.  This is how we build on jenkins.osmocom.org
+#
+# environment variables:
+# * WITH_MANUALS: build manual PDFs if set to "1"
+# * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
+#
 
 if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
 	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
@@ -22,9 +27,17 @@ verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
+export PATH="$inst/bin:$PATH"
 
 osmo-build-dep.sh libosmocore "" ac_cv_path_DOXYGEN=false
 osmo-build-dep.sh libosmo-abis
+
+# Additional configure options and depends
+CONFIG=""
+if [ "$WITH_MANUALS" = "1" ]; then
+	osmo-build-dep.sh osmo-gsm-manuals
+	CONFIG="--enable-manuals"
+fi
 
 set +x
 echo
@@ -36,9 +49,13 @@ set -x
 
 cd "$base"
 autoreconf --install --force
-./configure --enable-sanitize --enable-external-tests --enable-werror
+./configure --enable-sanitize --enable-external-tests --enable-werror $CONFIG
 $MAKE $PARALLEL_MAKE
 $MAKE check || cat-testlogs.sh
-$MAKE distcheck || cat-testlogs.sh
+DISTCHECK_CONFIGURE_FLAGS="$CONFIG" $MAKE distcheck || cat-testlogs.sh
+
+if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
+	make -C "$base/doc/manuals" publish
+fi
 
 osmo-clean-workspace.sh
