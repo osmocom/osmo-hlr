a5e9ca904561b3655af677d3e5d79e20b8105cba
Harald Welte <laforge@gnumonks.org>
2019-08-21 20:01:31 +0200

    AUC: Add support for setting the AMF separation bit to '1' for EUTRAN
    Change-Id: Ic766bc40f6126bb479bd0a05b0e96bec3e240008

--- src/hlr_vty_subscr.c
+++ src/hlr_vty_subscr.c
@@ -628,7 +628,7 @@ DEFUN(subscriber_rat,
 	const char *id = argv[1];
 	const char *rat_str = argv[2];
 	const char *allowed_forbidden = argv[3];
-	enum osmo_rat_type rat;
+	enum osmo_rat_type rat = OSMO_RAT_UNKNOWN;
 	bool allowed;
 	int rc;
 
@@ -636,6 +636,8 @@ DEFUN(subscriber_rat,
 		rat = OSMO_RAT_GERAN_A;
 	else if (strcmp(rat_str, "utran-iu") == 0)
 		rat = OSMO_RAT_UTRAN_IU;
+	else if (strcmp(rat_str, "eutran") == 0)
+		rat = OSMO_RAT_EUTRAN_SGS;
 
 	allowed = (strcmp(allowed_forbidden, "allowed") == 0);
 
