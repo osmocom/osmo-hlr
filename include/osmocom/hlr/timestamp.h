6aa871db71a9e6adbf347a4bc26e3561a3292adc
Neels Hofmeyr <neels@hofmeyr.de>
2019-11-20 03:35:37 +0100

    D-GSM 1/n: add mslookup server in osmo-hlr
    Implement the mslookup server to service remote mslookup requests.
    
    This patch merely adds the logic to answer incoming mslookup requests, an
    actual method to receive requests (mDNS) follows in a subsequent patch.
    
    - API to configure service names and addresses for the local site (per MSC).
    - determine whether a subscriber is on a local MSC
      (checking the local proxy will be added in subsequent patch that adds proxy
      capability).
    - VTY config follows in a subsequent patch.
    
    For a detailed overview of the D-GSM and mslookup related files, please see the
    elaborate comment at the top of mslookup.c (already added in an earlier patch).
    
    Change-Id: Ife4a61d71926d08f310a1aeed9d9f1974f64178b

---
+++ include/osmocom/hlr/timestamp.h
@@ -0,0 +1,28 @@
+/* Copyright 2019 by sysmocom s.f.m.c. GmbH <info@sysmocom.de>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <sys/time.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef time_t timestamp_t;
+void timestamp_update(timestamp_t *timestamp);
+bool timestamp_age(const timestamp_t *timestamp, uint32_t *age);
