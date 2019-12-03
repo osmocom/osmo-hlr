80dc9ae4be63774dbfb95a54cc2672b89cf21259
Harald Welte <laforge@osmocom.org>
2019-12-03 22:16:39 +0100

    hlr: exit(2) on unsupported positional arguments on command line
    Change-Id: I0c2738d380a7e79622fb3db2062c17782555e82d

--- src/hlr.c
+++ src/hlr.c
@@ -792,6 +792,11 @@ static void handle_options(int argc, char **argv)
 			break;
 		}
 	}
+
+	if (argc > optind) {
+		fprintf(stderr, "Unsupported positional arguments on command line\n");
+		exit(2);
+	}
 }
 
 static void signal_hdlr(int signal)
